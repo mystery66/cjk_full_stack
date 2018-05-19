780b84d087d2d8b95fede08dabee55eccdb692b5
mystery66 <1317671142@qq.com>
2018-05-19 10:14:55 +0800

    表单/图片增删

--- wxapp/wxapp-starter/config/api.js
+++ wxapp/wxapp-starter/config/api.js
@@ -1,1 +1,2 @@
-export const API_BASE = 'https://www.easy-mock.com/mock/5afc2aa8e5c64d22cc1ca4e1/cjk/cjk#!method=get'
+export const API_BASE = 'https://www.easy-mock.com/mock/5afc2aa8e5c64d22cc1ca4e1/cjk/cjk#!method=get';
+export const API_CREATE ='https://www.easy-mock.com/mock/5af03ac5957fd4308a30b9c0/api/create#!method=get'
