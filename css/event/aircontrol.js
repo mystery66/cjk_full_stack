5cf15ff7b60a97d5afc46d9593dc275ec944db26
mystery66 <1317671142@qq.com>
2018-05-29 14:26:32 +0800

    已完成

---
+++ css/event/aircontrol.js
@@ -0,0 +1,24 @@
+const TV = {
+    open:function() {
+        console.log('打开电视');
+    },
+    close: function() {
+        console.log('关闭电视');
+    }
+}
+
+const AirConditional = {
+    // 统一的接口
+    open:function() {
+        console.log('打开空调');
+    },
+    close: function() {
+        console.log('关闭空调');
+    }
+}
+
+function setCommand(objet) {
+  object.type = object;
+  type.open;
+  type.close;
+}
