356282bb5309bea56b78b234b34d4367d233cee6
mystery66 <1317671142@qq.com>
2018-06-05 10:14:05 +0800

    已完成

--- wxapp/jinritoutiao/pages/userinfo/userinfo.js
+++ wxapp/jinritoutiao/pages/userinfo/userinfo.js
@@ -1,35 +1,29 @@
 // pages/userinfo/userinfo.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    userInfo:{  
-      avatarUrl:"", //用户头像  
-      nickName:"", //用户昵称  
-    }  
+    //用户个人信息  
+    userInfo: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var that=this;  
-    /**  
-     * 获取用户信息  
-     */  
-    wx.getUserInfo({  
-      success:function(res){  
-        console.log(res);  
-        var avatarUrl = 'userInfo.avatarUrl';  
-        var nickName = 'userInfo.nickName';  
-        that.setData({  
-          [avatarUrl]: res.userInfo.avatarUrl,  
-          [nickName]:res.userInfo.nickName,  
-        })  
-      }  
-    })  
+    wx.getUserInfo({
+      success: res => {
+        app.globalData.userInfo = res.userInfo
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    })
+    
   },
 
   /**
