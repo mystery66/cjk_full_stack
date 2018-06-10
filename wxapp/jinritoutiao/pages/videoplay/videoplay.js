9d96f41181b1865e7f36c0bee9ad4ac818eef676
mystery66 <1317671142@qq.com>
2018-06-10 20:30:51 +0800

    添加了视频播放

---
+++ wxapp/jinritoutiao/pages/videoplay/videoplay.js
@@ -0,0 +1,105 @@
+// pages/videoplay/videoplay.js
+Page({
+   
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    image: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2729126594,811067681&fm=27&gp=0.jpg',
+    icon: '/images/icon_newsicon.png',
+    publisher: '乐娱网',
+    video: 'http://v11-tt.ixigua.com/bf314894f3be6a2ad14319cfe25e2a8f/5b1bcc8f/video/m/220b8a858ea3cc94d2db79b27f93d2c8fed1157f5aa00003455e557d37a/',
+   comment: [{
+    image: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3738611942,2062433065&fm=27&gp=0.jpg',
+    username: 'Hello World',
+    num: '22',
+    usercomment: '赞！赞！赞！',
+    time: '06-07  20:03',
+
+  },
+  {
+    image: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2233050185,361857994&fm=27&gp=0.jpg',
+    username: '向往阳光',
+    num: '90',
+    usercomment: '经典',
+    time: '06-07  17:46',
+  },
+  {
+    image: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3536822752,2201330562&fm=27&gp=0.jpg',
+    username: '飞翔',
+    num: '35',
+    usercomment: 'nice!好看！',
+    time: '06-08  08:00',
+  },
+  {
+    image: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1238306461,4175201788&fm=27&gp=0.jpg',
+    username: 'dreaming',
+    num: '90',
+    usercomment: '星爷！',
+    time: '06-08  02:46',
+  },
+  {
+    image: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2885719463,3098246584&fm=27&gp=0.jpg',
+    username: '心似火',
+    num: '90',
+    usercomment: '干得漂亮',
+    time: '06-08  05:30',
+  }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
